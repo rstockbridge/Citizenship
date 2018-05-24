752dd568f21377c241537a3787e20bf9958d7534
Rebecca Stockbridge <rebecca.stockbridge@gmail.com>
2018-05-24 14:58:56 -0400

    Add ViewPager capability
    - Replace FlashcardActivity with FlashcardPagerActivity
    - Create custom NonSwipeableViewPager class that prevents swiping
    - Enable page update when "Next" button is clicked
    - Enable "medium" and "long" buttons in MainActivity and pass desired
      practice length to FlashcardPagerActivity
    - Dummy data is still being used for each flashcard page.

---
+++ app/src/main/java/com/github/rstockbridge/flashcards/NonSwipeableViewPager.java
@@ -0,0 +1,27 @@
+package com.github.rstockbridge.flashcards;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(final Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(final MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(final MotionEvent event) {
+        return false;
+    }
+}
