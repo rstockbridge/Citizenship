9de485b0d626faf8d0ad439b9827470c931069aa
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-06-01 15:43:36 -0400

    Move FavoriteHolder into FavoriteAdapter

--- app/src/main/java/com/github/rstockbridge/citizenship/FavoriteAdapter.java
+++ app/src/main/java/com/github/rstockbridge/citizenship/FavoriteAdapter.java
@@ -4,13 +4,17 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.github.rstockbridge.citizenship.data.FavoritesStorage;
 import com.github.rstockbridge.citizenship.data.Question;
 
 import java.util.List;
 
-public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteHolder> {
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.FavoriteHolder> {
     private Fragment fragment;
     private List<Question> questions;
 
@@ -35,5 +39,44 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteHolder> {
     @Override
     public int getItemCount() {
         return questions.size();
+    }
+
+    class FavoriteHolder extends RecyclerView.ViewHolder {
+        private TextView questionLabel;
+        private ImageButton favoriteButton;
+
+        FavoriteHolder(final LayoutInflater inflater, final ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_favorite, parent, false));
+
+            questionLabel = itemView.findViewById(R.id.question_label_list);
+            favoriteButton = itemView.findViewById(R.id.make_favorite_list);
+        }
+
+        void bind(final Question question) {
+            questionLabel.setText(question.getQuestionText());
+
+            favoriteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (questionIsFavorite(question)) {
+                        FavoritesStorage.getSharedInstance().removeFromFavorites(question.getId());
+                    } else {
+                        FavoritesStorage.getSharedInstance().addToFavorites(question.getId());
+                    }
+
+                    syncFavoriteButton(question);
+                }
+            });
+
+            syncFavoriteButton(question);
+        }
+
+        private void syncFavoriteButton(final Question question) {
+            favoriteButton.setImageResource(questionIsFavorite(question) ? R.drawable.ic_favorite_filled : R.drawable.ic_favorite_border);
+        }
+
+        private boolean questionIsFavorite(final Question question) {
+            return FavoritesStorage.getSharedInstance().contains(question.getId());
+        }
     }
 }
